385ac9d96ec245e460c662065f011e0b41d73b9d
wu06617106 <wu06617106@gmail.com>
2018-04-27 09:34:07 +0800

    Create NHIICCardReader.h

---
+++ NHIICCardReader.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#ifdef NHIICCARDREADER_EXPORTS
+#define NHIICCARDREADER_API __declspec(dllexport)
+#else
+#define NHIICCARDREADER_API __declspec(dllimport)
+#endif
+
+struct NHIInfo
+{
+	string name = "";
+	string id = "";
+	string birthDate = "";
+	string gender = "";
+};
+
+class NHIICCARDREADER_API NHIICCardReader
+{
+public:
+	static int ReadSCardInfo(NHIInfo &icInfo);
+private:
+	static NHIInfo SplitInfoString(char const*info, int length);
+	static string RetrieveStringByIndex(char const*info, int startIndex, int endIndex, int length);
+};
